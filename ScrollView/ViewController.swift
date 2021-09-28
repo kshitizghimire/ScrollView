d117f4629073304a7f5c8f2961f6c5dcc5eaff16
Tiz <kshitiz.ghimire@gmail.com>
2021-09-28 18:33:01 +1000

    Add more subview

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -1,32 +1,24 @@
-//
-//  ViewController.swift
-//  ScrollView
-//
-//  Created by Tiz on 28/9/21.
-//
-
 import UIKit
 
 class ScrollView: UIView {
-    
     var contentSize: CGSize = .zero
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
-        self.addGestureRecognizer(pan)
+        addGestureRecognizer(pan)
     }
-    
-    required init?(coder: NSCoder) {
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     @objc func handlePanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
         let point = gestureRecognizer.translation(in: self)
 
         var bounds = self.bounds
-        
-        
+
         let newY = bounds.origin.y - point.y
         let minY = 0 as CGFloat
         let maxY = contentSize.height - bounds.size.height
@@ -38,25 +30,24 @@ class ScrollView: UIView {
 }
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         let scrollView = ScrollView(frame: view.bounds)
         scrollView.contentSize = CGSize(width: view.bounds.width, height: view.bounds.height * 2)
-        
-        let red = UIView(frame: CGRect(x: 10, y: 10, width: 20, height: 30))
+
+        let red = UIView(frame: CGRect(x: 100, y: 100, width: 200, height: 30))
         red.backgroundColor = .red
-        
-        let green = UIView(frame: CGRect(x: 40, y: 60, width: 200, height: 300))
+
+        let green = UIView(frame: CGRect(x: 40, y: 160, width: 200, height: 300))
         green.backgroundColor = .green
-        
-        
+
+        let yellow = UIView(frame: CGRect(x: 150, y: 800, width: 100, height: 100))
+        yellow.backgroundColor = .yellow
+
         scrollView.addSubview(red)
         scrollView.addSubview(green)
-        
-        self.view.addSubview(scrollView)
+        scrollView.addSubview(yellow)
+
+        view.addSubview(scrollView)
     }
-
-
 }
-
